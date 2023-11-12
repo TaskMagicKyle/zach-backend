1e56b703f0b464c22bdff7692eddd5dd8b3fcc1c
kyle wilson <kyle@wilsondevelops.com>
2023-11-12 14:06:17 +0100

    add csv

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,15 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+router.post('/csv', async function(req, res, next) {
+  const { body } = req;
+  console.log(body)
 
+  if(body.url) {
+    const {data} = await axios.get(body.url)
+    res.send(data);
+  }else {
+    res.send('respond with a resource');
+  }
+});
 module.exports = router;
